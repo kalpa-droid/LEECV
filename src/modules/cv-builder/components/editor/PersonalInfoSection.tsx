99575b81b9ff26276224052bb1af8f63240c735e
kalpa-droid <kalpagrafica@gmail.com>
2026-09-02 21:13:33 -0300

    refactor(ui): remove manual adjustment section control for Titular Profesional as it is permanently anchored under header name

--- src/modules/cv-builder/components/editor/PersonalInfoSection.tsx
+++ src/modules/cv-builder/components/editor/PersonalInfoSection.tsx
@@ -242,10 +242,6 @@ export default function PersonalInfoSection({ onOpenPhotoCropper }: { onOpenPhot
                 placeholder="Ej: Profesora de Lengua y Literatura | Referente en Innovación Educativa"
               />
 
-              {/* Ajuste Manual: Titular Profesional / Frase */}
-              <div className="pt-2 border-t border-[var(--color-neutral-border)]">
-                <SectionManualAdjustment sectionId="frase" cvData={cvData} setCvData={setCvData} />
-              </div>
             </div>
           </PanelSection>
         </div>
